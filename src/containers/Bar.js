2434b02fb17afb4e3d68ff273f05cf5806546efd
Noone <zhu3521953@163.com>
2018-04-13 19:26:34 +0800

    static

--- src/containers/Bar.js
+++ src/containers/Bar.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { fetchPostsIfNeeded, clearData } from '../actions/count.js'
 
 export default class Bar extends Component {
     render () {
         return (
-            <div>Bar</div>
+            <div>
+                
+            </div>
         )
     }
 }
