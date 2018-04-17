3e2997d9bf8d95a2a9ef3536aab20233999030ef
Noone <zhu3521953@163.com>
2018-04-17 14:10:08 +0800

    antd first experience

--- src/containers/Antd.js
+++ src/containers/Antd.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+import {Collapse} from 'antd'
+const Panel = Collapse.Panel
+
+const text = `
+    this is a react-demo by antd : )
+`
 
 export default class Antd extends Component {
     render () {
         return (
-            <div>antd</div>
+            <Collapse defaultActiveKey={['1']}>
+            <Panel header="This is panel header 1" key="1">
+              <p>{text}</p>
+            </Panel>
+            <Panel header="This is panel header 2" key="2">
+              <p>{text}</p>
+            </Panel>
+            <Panel header="This is panel header 3" key="3" disabled>
+              <p>{text}</p>
+            </Panel>
+          </Collapse>
         )
     }
 }
